f87bdb64e0fb93c9b45e2f8cedc5e4fdc475b9da
Matias Ros <matias.ros>
2023-03-03 15:08:45 -0300

    se realizan configuraciones necesarias para apache

--- src/app/modulos/transloco-root/transloco-root.module.ts
+++ src/app/modulos/transloco-root/transloco-root.module.ts
@@ -15,7 +15,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`assets/i18n/${lang}.json`);
   }
 }
 
@@ -27,9 +27,10 @@ export class TranslocoHttpLoader implements TranslocoLoader {
       useValue: translocoConfig({
         availableLangs: ['en', 'es'],
         defaultLang: 'en',
+        fallbackLang: 'en',
         // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
-        prodMode: environment.production,
+        prodMode: false
       })
     },
     { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader }
