6f98b33ad1148c988d43aa8adb8456bd47777671
Naser fernandez <naser_015@hotmail.com>
2023-03-30 12:11:21 -1100

    Se agregan validaciones

--- src/app/modulos/principal/locales/locales-adheridos/locales-crear/locales-crear.component.ts
+++ src/app/modulos/principal/locales/locales-adheridos/locales-crear/locales-crear.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
-
 @Component({
   selector: 'app-locales-crear',
   templateUrl: './locales-crear.component.html',
@@ -11,44 +10,46 @@ export class LocalesCrearComponent implements OnInit {
 
   myForm: FormGroup;
   emailFormControl: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  formInvalid = false;
 
+  get nombreControl() { return this.myForm.get('nombre'); }
+  get rubroControl() { return this.myForm.get('rubro'); }
+  get fechaControl() { return this.myForm.get('fecha'); }
 
   constructor(private fb: FormBuilder) {
-    this.myForm = this.fb.group
-      ({
-
-      })
-
+    this.myForm = this.fb.group({});
   }
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
       nombre: ['', Validators.required],
-      rubro: [''],
+      rubro: ['', Validators.required],
       sitioWeb: [''],
-      fecha: [new Date()],
+      fecha: [new Date(), Validators.required],
       destacado: [false],
       mostrarEnApp: [false],
       emailFormControl: this.emailFormControl,
-      tipoLocal: [''],
-
+      tipoLocal: ['', Validators.required],
     });
   }
-  guardar() {
 
-    const formValues = this.myForm.getRawValue();
-    const jsonStr = JSON.stringify(formValues);
-    console.log(jsonStr);
-    alert("El formulario se ha guardado correctamente.");
+  guardar() {
+    if (this.myForm.valid) {
+      const formValues = this.myForm.getRawValue();
+      const jsonStr = JSON.stringify(formValues);
+      console.log(jsonStr);
+    } else {
+      this.formInvalid = true;
+      this.myForm.markAllAsTouched(); 
+    }
   }
+
   cancelar() {
     this.myForm.reset();
   }
+
   formControlCompleted(controlName: string): boolean | null {
     const control = this.myForm.get(controlName);
     return control && control.valid && (control.value !== null && control.value !== '');
   }
-  
-
-  
 }
